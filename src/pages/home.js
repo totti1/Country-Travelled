218d09127bc242e56a389364f87ff2e3d1339e58
Aristote Katy <stotestorch@gmail.com>
2021-12-10 00:08:39 +0200

    fetching data in home.js file

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect }  from 'react';
 const Home = () => {
-return(
-        <div>
-            <h1>Home Page</h1>
-        </div>
-    );
-}
+    // fetch('https://restcountries.com/v3.1/all')
+    //     .then(response => response.json())
+    //     .then(data => console.log(data));
+    const [error, setError] = useState(null);
+        const [isLoaded, setIsLoaded] = useState(false);
+        const [countries, setUsers] = useState([]);
+        useEffect(() => {
+            fetch("https://restcountries.com/v3.1/all")
+                .then(res => res.json())
+                .then(
+                    (data) => {
+                        setIsLoaded(true);
+                        setUsers(data);
+                    },
+                    (error) => {
+                        setIsLoaded(true);
+                        setError(error);
+                    }
+                )
+            }, [])
+    if (error) {
+            return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+            return <div>Loading...</div>;
+        } else {
+            return (
+                <ul>
+                    {countries.map(item => (
+                    <li>
+                        {item.name.common} 
+                    </li>
+                    ))}
+                </ul>
+            );
+        }
+    }
+
+// return(
+//         <div>
+//             <h1>Home Page</h1>
+//         </div>
+//     );
+// }
 export default Home;
